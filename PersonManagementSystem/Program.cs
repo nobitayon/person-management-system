9883af5dfb55c14e010ea3b3211a7e1b45f0455e
yonathanm10 <yonathanm@cocoindonesia.com>
2024-06-04 19:55:31 +0700

    person class

---
+++ PersonManagementSystem/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonManagementSystem
+{
+    class Program
+    {
+        class Person 
+        {
+            public string name {get; protected set;}
+            public int age {get; protected set;}
+
+            public Person(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
+
+            public void setName(string name)
+            {
+                this.name = name;
+            }
+
+            public void setAge(int age)
+            {
+                this.age = age;
+            }
+
+            public string returnDetails()
+            {
+                return name + " - " + age;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Person person = new Person("Ab",23);
+
+            Console.WriteLine(person.returnDetails());  
+
+            person.setName("yon");
+            person.setAge(24);   
+
+            Console.WriteLine(person.returnDetails()); 
+
+            Console.ReadLine();  
+        }
+    }
+}
